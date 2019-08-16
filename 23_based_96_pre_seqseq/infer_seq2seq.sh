ed032a3d9c74cb482dc7056c1d0c0159094fe181
sample <love2poppy@gmail.com>
2019-08-16 11:20:12 +0800

    修改使用模型的参数

--- 23_based_96_pre_seqseq/infer_seq2seq.sh
+++ 23_based_96_pre_seqseq/infer_seq2seq.sh
@@ -1,9 +1,9 @@
 DIR=./biwei-dialog0
 python3 ${DIR}/infer_seq2seq.py \
     -test_data /mnt/mnt8tsdc/limiaojin/23_based_96_pre_seqseq/data/weibo/unique_src.txt \
-    -test_out ./out/23_out \
+    -test_out ./out/24_out \
     -vocab ./data/weibo/weibo.vocab.pkl \
-    -seq2seq /mnt/mnt8tsdc/limiaojin/23_based_96_pre_seqseq/out/checkpoint_epoch9.pkl \
+    -seq2seq ./out/checkpoint_epoch9.pkl \
     -sampler /mnt/mnt8tsdc/limiaojin/96_pre_sampler/96_out/sampler/checkpoint_epoch9.pkl \
     -config ./config.yml \
     -beam_size 3 \
